4ea5ca33418a04ab5d4a9f78d4a979e11a0eed84
Fady El Gawly <fgawly@gmail.com>
2020-09-14 12:18:10 +0200

    Initial Commit

---
+++ Faylasof Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Faylasof Task
+//
+//  Created by Fady Hanna on 9/14/20.
+//  Copyright © 2020 Fady Hanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
